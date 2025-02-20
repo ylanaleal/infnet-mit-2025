304ae3353fdf8bb3d85e8666eed8a0b339579c37
Ylana Oliveira <ylanalmo@gmail.com>
2025-02-20 09:26:34 -0300

    setup aulas

---
+++ 1.frontend-com-react/aulas/aplicações-react-com-controle-de-estado.js
@@ -0,0 +1,11 @@
+//capítulo 3 do livro “Pro MERN Stack”
+
+//React Components
+//React components can be composed using other components and basic HTML elements; they can respond to user input, change state, interact with other components, and much more.
+
+//React Classes
+//React classes are used to create real components.
+//These classes can then be reused within other components, handle events, and so much more.
+//React classes are created by extending React.Component, the base class from which all custom classes must be derived. Within the class definition, at the minimum, a render() method is needed. This method is what React calls when it needs to display the component in the UI.
+//But render() is one that must be present, otherwise the component will have no screen presence. The render() function is supposed to return an element (which can be either a native HTML element such as a <div> or an instance of another React component).
+
