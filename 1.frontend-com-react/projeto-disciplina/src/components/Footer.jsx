df09487b8becfb6157770ad48f6cd2a179d39c5b
Ylana Oliveira <ylanalmo@gmail.com>
2025-02-24 19:03:45 -0300

    projeto disciplina finalizado

--- 1.frontend-com-react/projeto-disciplina/src/components/Footer.jsx
+++ 1.frontend-com-react/projeto-disciplina/src/components/Footer.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterStyle = styled.div`
+  font-family: monospace;
+  background: #a3b7a9;
+  margin-bottom: 0;
+  padding: 5px;
+  text-align: center;
+`;
 
 const Footer = () => {
   return (
-    <div>
-      <p
-        style={{
-          background: '#A3B7A9',
-          margin: '0',
-          padding: '5px',
-          textAlign: 'center',
-        }}
-      >
-        Todos os direitos reservados.
-      </p>
-    </div>
+    <FooterStyle>
+      <p style={{ marginBottom: '0' }}>Todos os direitos reservados.</p>
+    </FooterStyle>
   );
 };
 
